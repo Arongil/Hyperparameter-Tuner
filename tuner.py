b6c95f5fd05eeeefaae8c3862178dbd528e599d1
Arongil <lakernewhouse@gmail.com>
2018-05-24 05:22:21 -0900

    Update tuner.py

--- tuner.py
+++ tuner.py
@@ -5,14 +5,31 @@ import optimization.newton as newton
 import optimization.quasi_newton as qn
 import RNN as rnn
 
-
 def networkLoss(v):
-  hypotheticalRNN = rnn.RNN(60, 20, v[0])
-  return hypotheticalRNN.train(10)
+  averageLoss = 0
+  for i in range(100): # v[0] and v[1] must be whole numbers
+    hypotheticalRNN = rnn.RNN(v[0], v[1], v[2])
+    hypotheticalRNN.initAdagrad()
+    averageLoss += hypotheticalRNN.train(1000)/v[1]
+  return averageLoss
+
+def continuousNetworkLoss(v):
+  print(v)
+  # To make the discrete inputs of hidden_size and seq_length continuous for the loss function,
+  # take their weighted averages between the two closest integers.
+  lowHiddenWeight  = 1 - (v[0] - np.floor(v[0]))
+  highHiddenWeight = v[0] - np.floor(v[0])
+  lowSeqWeight  = 1 - (v[1] - np.floor(v[1]))
+  highSeqWeight = v[1] - np.floor(v[1])
+  discreteLowHidden  = lowHiddenWeight * (lowSeqWeight*networkLoss([int(np.floor(v[0])), int(np.floor(v[1])), v[2]]) +
+                                         highSeqWeight*networkLoss([int(np.floor(v[0])), int(np.floor(v[1])) + 1, v[2]]))
+  discreteHighHidden = highHiddenWeight * (lowSeqWeight*networkLoss([int(np.floor(v[0])) + 1, int(np.floor(v[1])), v[2]]) +
+                                          highSeqWeight*networkLoss([int(np.floor(v[0])) + 1, int(np.floor(v[1])) + 1, v[2]]))
+  return discreteLowHidden + discreteHighHidden
 
 class HPT: # HyperparameterTuner
 
-  def __init__(self, network, tuning_rate = 0.01, training_steps = 1000, optimizer=sd.optimize):
+  def __init__(self, network, tuning_rate = 01e-2, training_steps = 1e3, optimizer=sd.optimize):
     '''
     Hyperparameter tuners find the best combination of hyperparameters to make RNNs learn the fastest.
     They pick a random combination of hyperparameters, first, and perform gradient descent to minimize loss after a constant number of training steps.
@@ -25,14 +42,15 @@ class HPT: # HyperparameterTuner
     self.optimizer = optimizer
 
   def tune(self):
-    # apply Gradient Descent on randomly initialized network hyperparameters. (Tune a hyperparameter tuner??)
+    # apply Gradient Descent on randomly initialized network hyperparameters.
     # note: RNN.train returns the loss of the network following the training.
-    # This loss, however, increases as the network improves. Therefore, add the gradient to maximize.
-    return self.optimizer(networkLoss, 1, maxSteps=40, lowerBound = 0, stepSize=0.01)
+    return self.optimizer(continuousNetworkLoss)
 
 character_model = rnn.RNN()
 # character_model.train(1000)
 # character_model.sampleN(200)
 
-hyperparameterTuner = HPT(character_model, optimizer=sd.optimize)
+hyperparameterTuner = HPT(character_model, optimizer = lambda net: sd.optimize
+                         (net, 3, maxSteps=20, stepSize=0.05, lowerBound = 1, upperBound = 10, convergence=1e-4, maximize=False))
 optimum = hyperparameterTuner.tune()
+print("The optimum is %s." %(optimum))
